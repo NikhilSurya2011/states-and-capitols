691152633dc1a307ff511a494c87311fe28e722e
NikhilSurya2011 <66034942+NikhilSurya2011@users.noreply.github.com>
2020-10-16 11:12:02 -0700

    Create states and capitols.py

---
+++ states and capitols.py
@@ -0,0 +1,72 @@
+
+
+import random
+#list for capitols
+capitals={
+    'Alabama': 'Montgomery',
+    'Alaska': 'Juneau',
+    'Arizona':'Phoenix',
+    'Arkansas':'Little Rock',
+    'California': 'Sacramento',
+    'Colorado':'Denver',
+    'Connecticut':'Hartford',
+    'Delaware':'Dover',
+    'Florida': 'Tallahassee',
+    'Georgia': 'Atlanta',
+    'Hawaii': 'Honolulu',
+    'Idaho': 'Boise',
+    'Illinios': 'Springfield',
+    'Indiana': 'Indianapolis',
+    'Iowa': 'Des Monies',
+    'Kansas': 'Topeka',
+    'Kentucky': 'Frankfort',
+    'Louisiana': 'Baton Rouge',
+    'Maine': 'Augusta',
+    'Maryland': 'Annapolis',
+    'Massachusetts': 'Boston',
+    'Michigan': 'Lansing',
+    'Minnesota': 'St. Paul',
+    'Mississippi': 'Jackson',
+    'Missouri': 'Jefferson City',
+    'Montana': 'Helena',
+    'Nebraska': 'Lincoln',
+    'Neveda': 'Carson City',
+    'New Hampshire': 'Concord',
+    'New Jersey': 'Trenton',
+    'New Mexico': 'Santa Fe',
+    'New York': 'Albany',
+    'North Carolina': 'Raleigh',
+    'North Dakota': 'Bismarck',
+    'Ohio': 'Columbus',
+    'Oklahoma': 'Oklahoma City',
+    'Oregon': 'Salem',
+    'Pennsylvania': 'Harrisburg',
+    'Rhoda Island': 'Providence',
+    'South Carolina': 'Columbia',
+    'South Dakoda': 'Pierre',
+    'Tennessee': 'Nashville',
+    'Texas': 'Austin',
+    'Utah': 'Salt Lake City',
+    'Vermont': 'Montpelier',
+    'Virginia': 'Richmond',
+    'Washington': 'Olympia',
+    'West Virginia': 'Charleston',
+    'Wisconsin': 'Madison',
+    'Wyoming': 'Cheyenne'  
+} 
+States=list(capitals.keys())
+print ("type exit to quit the game. answer the questions")
+point=0
+#loop
+while True:
+    state=random.choice(States) 
+    capital = capitals[state]
+    guess = input("what is the capital of %s?"%state )
+    if guess.casefold () == 'exit':  
+        break
+    elif guess.title() == capital:
+        point+=1
+        print('Correct! Your score is %d' %point)
+    else:
+        print('Incorrect. The capital of {} is {}.'.format(state,capital))
+print('Your final score is %d.' %point)
